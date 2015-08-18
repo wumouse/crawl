05b31612f8db74f781ab4efc11eee0a28f42f4aa
Wumouse <wumouse@qq.com>
2015-08-18 23:20:38 +0800

    去掉无用的action

--- app/Tasks/MainTask.php
+++ app/Tasks/MainTask.php
@@ -25,54 +25,5 @@ class MainTask extends TaskBase
      */
     public function mainAction()
     {
-        $options = $this->getOptions();
-        if (0 == $this->parameter->countOptions()) {
-            return $this->getInfoResponse($options);
-        }
-        $from = $this->parameter->getOption('f', 'chrome');
-        $to = $this->parameter->getOption('t');
-        if (!$to) {
-            return $this->getErrorResponse('invalid ' . $options->getDescription('t'));
-        }
-
-        $fromContentFile = $this->parameter->getOption('i');
-        if (!$fromContentFile) {
-            return $this->getErrorResponse('invalid ' . $options->getDescription('i'));
-        }
-        $fromContentFile = $this->convertPath($fromContentFile);
-        if (!stream_resolve_include_path($fromContentFile)) {
-            return $this->getErrorResponse('-i must be a exists file include request content');
-        }
-
-        $fromLogic = RequestFactory::get($from);
-        $toLogic = RequestFactory::get($to);
-
-        $request = $fromLogic->getRequest(file_get_contents($fromContentFile));
-        $toLogic->setRequest($request);
-
-        $output = $this->parameter->getOption('o');
-        $content = $toLogic->getContent();
-        if ($output) {
-            $output = $this->convertPath($output);
-            file_put_contents($output, $content);
-            return $this->getSuccessResponse('wrote to ' . realpath($output));
-        } else {
-            return $this->getSuccessResponse(PHP_EOL . $content);
-        }
-    }
-
-    /**
-     * 获取帮助信息
-     *
-     * @return Options
-     */
-    protected function getOptions()
-    {
-        $options = parent::getOptions();
-        $options->add('f', 'file format from');
-        $options->add('t', 'file format to be');
-        $options->add('i', 'file name of request file from');
-
-        return $options;
     }
 }
