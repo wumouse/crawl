2ced2b5d51066668dfe044998d807d46b9ab2c91
Wumouse <wumouse@qq.com>
2015-08-19 10:14:30 +0800

    重构代码

--- app/Library/Crawl/Adapter.php
+++ app/Library/Crawl/Adapter.php
@@ -51,58 +51,23 @@ abstract class Adapter
         if (is_string($uri)) {
             $this->setUri($uri);
         }
-        $this->handler = curl_init();
-    }
-
-    /**
-     *
-     *
-     * @param string $uri
-     * @return string
-     */
-    public function get($uri = null)
-    {
-        if ($uri) {
-            $this->setUri($uri);
-        }
-        $content = $this->exec();
-        $this->info = curl_getinfo($this->handler);
-        return $content;
     }
 
     /**
      * 设置URI
      *
      * @param string $uri
+     * @return Adapter
      */
-    private function setUri($uri)
-    {
-        $this->uri = $uri;
-        $this->options[CURLOPT_URL] = $uri;
-    }
+    abstract public function setUri($uri);
+
+    abstract public function getHttpCode();
 
     /**
-     * 获取HTTP 响应码
      *
-     * @return int
-     */
-    public function getHttpCode()
-    {
-        return $this->info['http_code'];
-    }
-
-    /**
-     * 执行
      *
+     * @param string $uri
      * @return string
      */
-    private function exec()
-    {
-        $options = $this->options;
-        $options[CURLINFO_HEADER_OUT] = true;
-        $options[CURLOPT_HEADER] = false;
-        $options[CURLOPT_RETURNTRANSFER] = true;
-        curl_setopt_array($this->handler, $options);
-        return curl_exec($this->handler);
-    }
+    abstract public function get($uri = null);
 }
--- app/Library/Crawl/Adapter/Curl.php
+++ app/Library/Crawl/Adapter/Curl.php
@@ -18,4 +18,60 @@ use Library\Crawl\Adapter;
 class Curl extends Adapter
 {
 
+    /**
+     * @inheritDoc
+     */
+    public function __construct($uri = null)
+    {
+        parent::__construct($uri);
+        $this->handler = curl_init();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function get($uri = null)
+    {
+        if ($uri) {
+            $this->setUri($uri);
+        }
+        $content = $this->exec();
+        $this->info = curl_getinfo($this->handler);
+        return $content;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setUri($uri)
+    {
+        $this->uri = $uri;
+        $this->options[CURLOPT_URL] = $uri;
+        return $this;
+    }
+
+    /**
+     * 获取HTTP 响应码
+     *
+     * @return int
+     */
+    public function getHttpCode()
+    {
+        return $this->info['http_code'];
+    }
+
+    /**
+     * 执行
+     *
+     * @return string
+     */
+    private function exec()
+    {
+        $options = $this->options;
+        $options[CURLINFO_HEADER_OUT] = true;
+        $options[CURLOPT_HEADER] = false;
+        $options[CURLOPT_RETURNTRANSFER] = true;
+        curl_setopt_array($this->handler, $options);
+        return curl_exec($this->handler);
+    }
 }
